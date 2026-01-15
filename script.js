cdd8c48f017270de286aa3042a8c44bd9eda0ff3
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-15 23:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ const openBtn = document.getElementById("openModal");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close-modal");
 
-// Open modal
 openBtn.addEventListener("click", () => {
   modal.classList.add("active");
 });
 
-// Close via button
 closeBtn.addEventListener("click", () => {
   modal.classList.remove("active");
 });
 
-// Close on outside click
 modal.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.classList.remove("active");
