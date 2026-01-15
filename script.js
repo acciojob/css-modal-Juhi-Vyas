602e938cb1bc5ff8e399f1767670bdb97c7f7aaa
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-15 14:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let modal = document.getElementById("modal")
+let btn1 = document.getElementById("openModal")
+let btn2 = document.getElementById("close-modal")
+
+btn1.addEventListener("click", () => {
+	
+})
