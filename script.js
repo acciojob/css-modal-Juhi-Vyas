a31684e45cbeaf9b94d3db8697c9941f225320f0
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-15 19:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 let openBtn = document.getElementById("openModal")
 let modal = document.querySelector(".modal")
+let modalContent = document.querySelector(".modal-content")
 let closeBtn = document.querySelector(".close-modal")
-let innerModal = document.querySelector(".modal-content")
 
-openBtn.addEventListener("click", () => {
-	innerModal.style.display = "block"
+openBtn.addEventListener("click", (e) => {
+  e.stopPropagation() 
+  modalContent.style.display = "block"
 })
 
 closeBtn.addEventListener("click", () => {
-	innerModal.style.display = "none"
+  modalContent.style.display = "none"
 })
 
-window.addEventListener('click', (e) => {   
-  if (!modal.contains(e.target)){
-	  innerModal.style.display = "none"
-  } 
-});
+window.addEventListener("click", (e) => {
+  if (!modalContent.contains(e.target)) {
+    modalContent.style.display = "none"
+  }
+})
+
 
 
